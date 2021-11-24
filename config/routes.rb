ccd045bf1002e3e4aafbda2ea05336b78723a90d
hunterMotko <hunterMotko@gmail.com>
2021-11-24 13:53:21 -0500

    semantic setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :chatrooms
+  resources :sessions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  root 'chatrooms#index'
 end
